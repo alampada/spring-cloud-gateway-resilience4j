7c0358b23537f13f80ee1fe3215d57a4100fe521
Antonios Lampadaridis <alampadaridis@expediagroup.com>
2021-11-23 15:05:06 +0000

    First commit

---
+++ src/main/java/com/example/gatewayresilience4j/GatewayResilience4jApplication.java
@@ -0,0 +1,13 @@
+package com.example.gatewayresilience4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayResilience4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayResilience4jApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gatewayresilience4j/GatewayResilience4jApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gatewayresilience4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayResilience4jApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
